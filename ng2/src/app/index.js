05b045a076b28ebe328aba1e3c3560b0d88f4c76
dagyrox <dagyrox@gmail.com>
2016-10-14 23:26:07 -0400

    add angular 2
    added angular 2 app, and pasted original index content within the ng2
    scafolded projects index.

---
+++ ng2/src/app/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var environment_1 = require('./environment');
+exports.environment = environment_1.environment;
+var ng2_component_1 = require('./ng2.component');
+exports.Ng2AppComponent = ng2_component_1.Ng2AppComponent;
